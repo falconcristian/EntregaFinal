8e4fac008fa4bbbb51bf9e326080223d709dbd16
Cristian Falcón <falcon.cristian@gmail.com>
2022-09-19 20:32:11 -0300

    Entrega parcial envio mail

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('dotenv').config();
+
+
+var indexRouter = require('./routes/index');
+var historiaRouter = require('./routes/historia')
+var galeriaRouter = require('./routes/galeria')
+var contactoRouter = require('./routes/contacto')
+var lentesvrRouter = require('./routes/lentesvr')
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/historia', historiaRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/contacto', contactoRouter);
+app.use('/lentesvr', lentesvrRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
