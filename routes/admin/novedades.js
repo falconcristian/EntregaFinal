87513adbdbef17ff05afea8b09a9a52cf02d93a0
Cristian Falcón <falcon.cristian@gmail.com>
2022-10-25 17:46:45 -0300

    actualización

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -20,5 +20,62 @@ router.get('/eliminar/:id', async (req,res,next) => {
 
 })
 
+router.get('/agregar', (req, res, next) =>{
+  res.render('admin/agregar',{
+    layout: 'admin/layout'
+  } )
+})
+
+router.post('/agregar', async (req,res,next) => {
+  try {
+    if (req.body.titulo != '' && req.body.subtitulo != '' && req.body.cuerpo != '') {
+      await novedadesModel.insertNovedad(req.body)
+      res.redirect('/admin/novedades')
+    } else{
+      res.render('admin/agregar',{
+        layout: "admin/layout",
+        error: true, message: "El campo titulo es obligatorio"
+      })
+    }
+    } catch (error) {
+      console.log(error)
+      res.render('admin/agregar', {
+        layout:"admin/layout",
+        error: true, message: "No se cargo la noticia"
+      })
+    }
+})
+
+router.get('/modificar/:id', async (req,res,next) => {
+
+  let id = req.params.id
+  let novedad = await novedadesModel.getNovedadById(id)
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+router.post('/modificar', async (req,res,next) => {
+  try{
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    
+    await novedadesModel.modificarNovedadById(obj, req.body.id)
+    res.redirect('/admin/novedades')
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: "No se modifico la noticia"
+    })
+  }
+}
+)
+
 
 module.exports = router;
